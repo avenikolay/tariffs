99bebbaa4c6156706589a18b376f06e11dfd2447
Nikolay Kozlov <nikolay@velkomb.com>
2020-01-23 16:42:29 +0500

    Initial commit

---
+++ precache-manifest.0868f8557655b0a242c0b62569567fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e90bd37b7576e618c58a453c0326f5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c5835e88c00af294a4",
+    "url": "/static/css/main.9a1d2387.chunk.css"
+  },
+  {
+    "revision": "b5bc3d37c73adb2943c0",
+    "url": "/static/js/2.c643a220.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c643a220.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26c5835e88c00af294a4",
+    "url": "/static/js/main.551985e1.chunk.js"
+  },
+  {
+    "revision": "37cd1baef18832e725c7",
+    "url": "/static/js/runtime-main.ca630dba.js"
+  }
+]);
